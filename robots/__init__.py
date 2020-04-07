dbe2934be75da5d36bbaf6f0fb464b40bc63da61
Andre <andre.burgaud@gmail.com>
2020-04-06 23:51:30 -0500

    Bumped to version 0.4.0

--- robots/__init__.py
+++ robots/__init__.py
@@ -6,6 +6,6 @@ from robots.parser import RobotsParser
 from robots.parser import RequestRate
 from robots.robotparser import RobotFileParser
 
-__version__ = "0.3.3"
+__version__ = "0.4.0"
 
 __all__ = ["RobotsParser", "RobotFileParser"]
--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ README = (CWD / "README.md").read_text()
 
 setup(
     name="robotspy",
-    version="0.3.3",
+    version="0.4.0",
     description="Robots Exclusion Protocol File Parser",
     long_description=README,
     long_description_content_type="text/markdown",
