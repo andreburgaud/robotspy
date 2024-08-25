85f85277017eec3eb9b8b7892ba2b19225d91144
Andre Burgaud <andre.burgaud@gmail.com>
2024-08-25 18:26:26 -0500

    bumped version

--- robots/__init__.py
+++ robots/__init__.py
@@ -6,6 +6,6 @@ from robots.parser import RobotsParser
 from robots.parser import RequestRate
 from robots.robotparser import RobotFileParser
 
-__version__ = "0.8.0"
+__version__ = "0.9.0"
 
 __all__ = ["RobotsParser", "RobotFileParser"]
