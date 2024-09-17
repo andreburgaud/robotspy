0eaa2303aac5f0b9afeb707292f6b23c7845f0db
Andre Burgaud <andre.burgaud@gmail.com>
2024-09-16 20:56:49 -0500

    added examples setting a timeout

--- examples/robots_url.py
+++ examples/robots_url.py
@@ -51,3 +51,37 @@ assert parser.can_fetch(
 assert parser.can_fetch("*", "http://www.musi-cal.com/")
 assert not parser.can_fetch("*", "http://www.musi-cal.com/wp-admin/")
 assert not parser.can_fetch("*", "/wp-admin/")
+
+# Examples with custom timeout
+parser = robots.RobotsParser.from_uri("https://robotspy.org/robots.txt", 2)
+
+if parser.errors:
+    print("ERRORS:")
+    print(parser.errors)
+
+if parser.warnings:
+    print("WARNINGS:")
+    print(parser.errors)
+
+assert parser.can_fetch(
+    "Googlebot", "https://robotspy.org/"
+)
+assert parser.can_fetch("*", "https://robotspy.org/")
+
+# Set a 0 timeout should result in an error
+
+parser = robots.RobotsParser.from_uri("https://robotspy.org/robots.txt", 0)
+assert parser.errors
+if parser.errors:
+    print("ERRORS:")
+    print(parser.errors)
+
+
+# Timeout error
+parser = robots.RobotsParser.from_uri("https://robotspy.org:555/robots.txt", 2)
+
+# The duration may be greater than the timeout because the urllib.request.urlopen timeout does not equate to a total timeout
+assert parser.errors
+if parser.errors:
+    print("ERRORS:")
+    print(parser.errors)
