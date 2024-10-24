19329e53b1265576b06ab234a37854a64dcaab8b
Andre Burgaud <andre.burgaud@gmail.com>
2024-10-23 22:51:35 -0500

    added file example demonstrating parser issue (infinite loop)

---
+++ examples/robots_file_large.py
@@ -0,0 +1,16 @@
+import robots
+
+AGENT = "Googlebot"
+
+parser = robots.RobotsParser.from_file("robots_file_large.txt")
+
+if parser.errors:
+    print("ERRORS:")
+    print(parser.errors)
+
+if parser.errors:
+    print("WARNINGS:")
+    print(parser.errors)
+
+assert parser.can_fetch("Googlebot", "/")
+assert not  parser.can_fetch("Exabot", "/")
